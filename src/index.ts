07d16f05cb5cf0ad2a03c1a4c4ae73097941e86d
Martin Petr <xpetr1@fi.muni.cz>
2020-09-22 00:40:46 +0200

    Socket io init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import "./assets/main.scss";
 import Game from "./Game";
 
+import * as io from "socket.io-client";
+
 const game = new Game();
 game.main();
+
+const socket = io("http://localhost:8080/");
+socket.on("connect", () => {
+  console.log("connected");
+});
