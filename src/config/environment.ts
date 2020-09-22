d4e3735e3bf1eff446a37307aa131e574c38ca9b
Martin Petr <martin.petr.276@gmail.com>
2020-09-22 20:36:04 +0200

    Socker server env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,7 @@
-const SOCKET_SERVER = "http://localhost:8080/";
+const SOCKET_SERVER =
+  process.env.mode === "production"
+    ? "https://lit-hamlet-62877.herokuapp.com/"
+    : "http://localhost:8080/";
 
 export default {
   SOCKET_SERVER,
